fb8357b3c66dae8b3b043477c487b6e1cd804233
Mary Lisova <mar.l1sova@yandex.ru>
2023-04-18 00:51:51 +0930

    ..

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -14,7 +14,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         print("Hello world for branch to merge")
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -28,5 +27,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    sleep(3)
+    return true
 }
 
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,6 +7,7 @@ final class MovieQuizViewController: UIViewController {
     }
 }
 
+
 /*
  Mock-данные
  
